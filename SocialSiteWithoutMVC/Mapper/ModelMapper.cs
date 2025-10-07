9e1d2fc9a883a44d1dc1b1abe5d1115d0faa0607
kapitanchikyt <stanislavkapchenov@mail.ru>
2025-10-07 17:04:15 +0300

    Improved the mapper code

--- SocialSiteWithoutMVC/Mapper/ModelMapper.cs
+++ SocialSiteWithoutMVC/Mapper/ModelMapper.cs
@@ -8,32 +8,34 @@ namespace SocialSiteWithoutMVC.Mapper;
 public static partial class ModelMapper
 {
     [MapperIgnoreSource(nameof(UserEntity.Password))]
-    [MapperIgnoreSource(nameof(UserEntity.Chats))]
-    [MapperIgnoreTarget(nameof(UserModel.Chats))]
     public static partial UserModel UserEntityToModel(UserEntity user);
     
-    [MapperIgnoreSource(nameof(ChatEntity.Id))]
-    [MapperIgnoreSource(nameof(ChatEntity.Users))]
-    [MapperIgnoreTarget(nameof(ChatModel.UsersLogin))]
-    public static partial ChatModel ChatEntityToModel(ChatEntity chat);
+    
+    [UserMapping(Default = true)]
+    public static ChatModel ChatEntityToModel(ChatEntity chat)
+    {
+        var model = ChatEntityToModelDefault(chat);
+        model.UsersLogin = chat.Users
+            .Select(u => u.Login)
+            .ToArray();
+        return model;
+    }
+    
     
     [MapperIgnoreSource(nameof(MessageEntity.Id))]
     [MapperIgnoreSource(nameof(MessageEntity.ChatId))]
     public static partial MessageModel MessageEntityToModel(MessageEntity message);
-
-    public static ChatModel ChatEntityToModel(ChatEntity chat, string thisLogin)
-    {
-        var model = ChatEntityToModel(chat);
-        model.UsersLogin = chat.Users
-            .Where(u => u.Login != thisLogin)
-            .Select(u => u.Login).ToArray();
-        return model;
-    }
-
-    public static ChatModel ChatEntityToModelWithoutMessages(ChatEntity chat, string thisLogin)
-    {
-        var model = ChatEntityToModel(chat, thisLogin);
-        model.Messages = null;
-        return model;
-    }
+    
+    
+    [MapperIgnoreSource(nameof(UserEntity.Password))]
+    [MapperIgnoreSource(nameof(UserEntity.Chats))]
+    [MapperIgnoreTarget(nameof(UserModel.Chats))]
+    public static partial UserModel UserEntityToModelWithoutChats(UserEntity user);
+    
+    
+    [MapperIgnoreSource(nameof(ChatEntity.Id))]
+    [MapperIgnoreSource(nameof(ChatEntity.Users))]
+    [MapperIgnoreTarget(nameof(ChatModel.UsersLogin))]
+    [UserMapping(Ignore = true)]
+    private static partial ChatModel ChatEntityToModelDefault(ChatEntity chat);
 }
