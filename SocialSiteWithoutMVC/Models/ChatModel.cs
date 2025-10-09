70493fc7646a462639c394e47205b230e3a08861
kapitanchikyt <stanislavkapchenov@mail.ru>
2025-10-09 16:44:23 +0300

    Edit nullable field to non-nullable

--- SocialSiteWithoutMVC/Models/ChatModel.cs
+++ SocialSiteWithoutMVC/Models/ChatModel.cs
@@ -2,7 +2,7 @@ namespace SocialSiteWithoutMVC.Models;
 
 public class ChatModel
 {
-    public string? Name { get; set; }
+    public string Name { get; set; } = null!;
     public string[] UsersLogin { get; set; } = null!;
     public MessageModel[]? Messages { get; set; } = null!;
 }
