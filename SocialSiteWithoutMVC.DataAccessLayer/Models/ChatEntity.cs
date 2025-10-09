77d96109c259c574ae723e52e66b0b779a545cc1
kapitanchikyt <stanislavkapchenov@mail.ru>
2025-10-09 17:57:56 +0300

    Add use Guid7

--- SocialSiteWithoutMVC.DataAccessLayer/Models/ChatEntity.cs
+++ SocialSiteWithoutMVC.DataAccessLayer/Models/ChatEntity.cs
@@ -2,7 +2,7 @@ namespace SocialSiteWithoutMVC.DataAccessLayer.Models;
 
 public record ChatEntity
 {
-    public Ulid Id { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; } = null!;
     public List<UserEntity> Users { get; set; } = null!;
     public List<MessageEntity>? Messages { get; set; }
